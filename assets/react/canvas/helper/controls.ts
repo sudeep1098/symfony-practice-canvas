31583a8a340ca3ae80c07ff0a1d1cba7d60d474b
Dark <sudeep1098@gmail.com>
2024-08-04 11:34:13 +0530

    added copy paste

--- assets/react/canvas/helper/controls.ts
+++ assets/react/canvas/helper/controls.ts
@@ -1,4 +1,4 @@
-import { deleteIcon } from "./icons";
+import { deleteIcon, copyIcon } from "./icons";
 
 export const buildControls = (fabric: any) => {
     fabric.Object.prototype.set({
@@ -12,8 +12,11 @@ export const buildControls = (fabric: any) => {
         strokeDashArray: [5, 5],
     });
 
-    const img = new Image();
-    img.src = deleteIcon;
+    const deleteImg = new Image();
+    deleteImg.src = deleteIcon;
+
+    const copyImg = new Image();
+    copyImg.src = copyIcon;
 
     function deleteObject(eventData: fabric.IEvent, transform: { target: fabric.Object }): boolean {
         const target = transform.target;
@@ -25,13 +28,32 @@ export const buildControls = (fabric: any) => {
         return false;
     }
 
+    function copyPaste(eventData: fabric.IEvent, transform: { target: fabric.Object }): boolean {
+        const target = transform.target;
+        const canvas = target.canvas;
+        if (canvas) {
+            target.clone((cloned: fabric.Object) => {
+                cloned.set({
+                    left: (target.left || 0) + 10,
+                    top: (target.top || 0) + 10,
+                    evented: true,
+                });
+                canvas.add(cloned);
+                canvas.setActiveObject(cloned);
+                canvas.requestRenderAll();
+            });
+        }
+        return false;
+    }
+
     function renderIcon(
         ctx: CanvasRenderingContext2D,
         left: number,
         top: number,
         styleOverride: Partial<fabric.Control>,
         fabricObject: fabric.Object,
-        cornerSize: number
+        cornerSize: number,
+        img: HTMLImageElement
     ): void {
         ctx.save();
         ctx.translate(left, top);
@@ -40,20 +62,30 @@ export const buildControls = (fabric: any) => {
         ctx.restore();
     }
 
-    const createDeleteControl = (cornerSize: number) => {
+    const createControl = (
+        cornerSize: number,
+        img: HTMLImageElement,
+        handler: (eventData: fabric.IEvent, transform: { target: fabric.Object }) => boolean,
+        x: number,
+        y: number,
+        offsetY: number
+    ) => {
         return new fabric.Control({
-            x: 0.5,
-            y: -0.5,
-            offsetY: 16,
+            x: x,
+            y: y,
+            offsetY: offsetY,
             cursorStyle: 'pointer',
-            mouseUpHandler: deleteObject,
+            mouseUpHandler: handler,
             render(ctx: CanvasRenderingContext2D, left: number, top: number, styleOverride: Partial<fabric.Control>, fabricObject: fabric.Object) {
-                renderIcon(ctx, left, top, styleOverride, fabricObject, cornerSize);
+                renderIcon(ctx, left, top, styleOverride, fabricObject, cornerSize, img);
             },
             cornerSize: cornerSize,
         });
     };
 
-    // Define the deleteControl
-    fabric.Object.prototype.controls.deleteControl = createDeleteControl(24);
+    const deleteControl = createControl(24, deleteImg, deleteObject, 0.5, -0.5, 16);
+    const copyPasteControl = createControl(24, copyImg, copyPaste, 0, -0.7, 10);
+
+    fabric.Object.prototype.controls.deleteControl = deleteControl;
+    fabric.Object.prototype.controls.copyPasteControl = copyPasteControl;
 };
